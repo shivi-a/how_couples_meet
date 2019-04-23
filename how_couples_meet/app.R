934ef8b0a89a11592a4869482a24512955e23cae
Shivani Aggarwal <saggarwal@college.harvard.edu>
2019-04-23 19:31:27 -0400

    Gathered dataframe to ensure that it is narrow rather than wide, allowing for manipulation of all the different meeting types

--- how_couples_meet/app.R
+++ how_couples_meet/app.R
@@ -23,16 +23,26 @@ ui <- fluidPage(
    sidebarLayout(
       sidebarPanel(
          radioButtons("region",
-                     "Region:", unique(couples_data$ppreg9))
+                     "Region:", unique(couples_data$ppreg9)),
+         selectInput("education",
+                     "Respondent Education Level:", unique(couples_data$ppeduc))
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
          tabsetPanel(
-           tabPanel("About",
-                    br(),
-                    h6("Thanks to data from the ", a("ShotSpotter Project", href="http://justicetechlab.org/shotspotter-data/"), " by the Justice Tech Lab, we can visualize gunshot data in Camden, NJ")
+           tabPanel("Earnings",
+                    h3("Earnings between Partners in the US"),
+                    plotOutput("incomePlot"),
+                    h4("The majority of men reported making more than their partners at all education levels. Women were more likely to report making less than their partner, except those with professional or doctorate degrees"),
+                    br()
                     ),
+           tabPanel("Age",
+                    h3("Correlations in Partner Ages"),
+                    plotOutput("agePlot"),
+                    h4("Points on the dashed line indicate respondents reporting a partner of the same age. Women tended to report older partners, while men reported younger partners"),
+                    br()
+           ),
            tabPanel("When Couples Meet", 
                     plotOutput("meetPlot")),
            tabPanel("Marry",
@@ -41,17 +51,25 @@ ui <- fluidPage(
       )
    )
 ),
-tabPanel("Meetings",
+tabPanel("Meet",
          mainPanel(
            h2("Meetings")
          )),
-tabPanel("Marriage",
+tabPanel("Marry",
          mainPanel(
-           h2("Marriage")
+           h2("Marriage"),
+           h4("When do couples marry?"),
+           h4("Among those married, how did they meet"),
+           h4("How old when you married")
+         )),
+tabPanel("Stay Together",
+         mainPanel(
+           h2("Stay Together"),
+           h4("The previous data involves six waves of data")
          )),
 tabPanel("About",
          mainPanel(
-           h2("SSDS Social Science Data Collection"),
+           h2("The Data"),
            h5("Thanks to data from the ", a("Social Science Data Collection", href="https://data.stanford.edu/hcmst2017"), " at Stanford University, we can understand how couples meet"),
            br(),
            h2("Source Code"),
@@ -62,6 +80,45 @@ tabPanel("About",
 # Define server logic required to draw a histogram
 
 server <- function(input, output) {
+  
+  output$incomePlot <- renderPlot({
+    
+    couples_data %>% 
+      
+      filter(ppeduc == input$education) %>% 
+    
+      mutate(Q23 = fct_relevel(Q23, 
+                               "We earned about the same amount",
+                               "[Partner Name] earned more", 
+                               "I earned more")) %>% 
+      
+      mutate(Q23 = fct_recode(Q23,"We earned about the same" = "We earned about the same amount", "My partner earned more" = "[Partner Name] earned more")) %>% 
+      
+      mutate(ppgender = fct_recode(ppgender, "Male Respondents" = "Male", "Female Respondents" = "Female")) %>% 
+      
+      # Removed those who refused from the dataset as well as those who reported that their partner was not working for pay -- a small total that do not contribute to any major trends
+      
+      filter(!is.na(Q23), 
+             Q23 != "Refused", 
+             Q23 != "[Partner Name] was not working for pay") %>%  
+      
+      # Create bar chart based on responses for Q23 (respondent's pay versus partner's pay)
+      
+      ggplot(aes(x = Q23, fill = ppgender)) + 
+      
+      geom_bar(show.legend=FALSE) + 
+      
+      facet_wrap(~ppgender) + 
+      
+      coord_flip() + 
+      
+      theme_few() + 
+      
+      scale_fill_manual(values = c("dodgerblue4", "deeppink4")) +
+      
+      labs(x = NULL, y = NULL)
+    
+  })
   
   output$meetPlot <- renderPlot({
 
@@ -88,6 +145,52 @@ server <- function(input, output) {
         labs(x = NULL, y = NULL)
     
    })
+  
+  output$agePlot <- renderPlot({
+    
+    # Create graphic to show the relationship between respondent's age and their partner's age -- are most reporting partners who are older than them? How does this differ according to the sex of the respondent?
+    
+    couples_data %>% 
+      
+      # Remove those who refused to answer this question (coded by a value of "-1")
+      filter(Q9 != -1) %>% 
+      
+      # Create a scatterplot of respondent's age versus partner's age, and colored by the respondent's gender
+      
+      ggplot(aes(x = ppage, y = Q9, color = ppgender)) +
+      
+      # Make points translucent to allow for visualization of overlapping points
+      
+      geom_point(alpha = 0.6) + 
+      
+      # Change the color scale to match more intuitive designations of male and female
+      
+      scale_color_manual(values = c("dodgerblue", "deeppink"), name = "Respondent's Sex") +
+      
+      # Add labels to contextualize the plot
+      
+      labs(x = "Respondent's Age", 
+           y = "Partner's Age") + 
+      
+      # Add a line with a slope of 1 -- points that lie on this line represent couples where the two are the same age -- above it, the partner is older, and below, the respondent is older
+      
+      geom_abline(slope = 1, linetype = "dashed") + 
+      
+      # Use theme_few for a cleaner plot
+      
+      theme_few() + 
+      
+      # Expand the limits of the default view so that the x and y axes are proportional, both showing 10 to 90
+      
+      expand_limits(x = 10:90) + 
+      
+      # Ensure that breaks are shown for the x and y axis in intervals of 10, as we tend to think of age in decades
+      
+      scale_x_continuous(breaks = seq(0, 100, by = 10)) + 
+      
+      scale_y_continuous(breaks = seq(0, 100, by = 10))
+  
+  })
   
   output$marryPlot <- renderPlot({
     
--- how_couples_meet_data.R
+++ how_couples_meet_data.R
@@ -8,7 +8,33 @@ library(fs)
 
 # Read in data from STATA dta file
 
-couples <- read.dta13("HCMST 2017 fresh sample for public sharing draft v1.1.dta")
+couples <- read.dta13("HCMST 2017 fresh sample for public sharing draft v1.1.dta") %>% 
+  mutate(ppeduc = fct_lump(ppeduc, 6, 
+                           other_level = "Never graduated high school"),
+         ppeduc = fct_recode(ppeduc, "High school graduate" = "HIGH SCHOOL GRADUATE - high school DIPLOMA or the equivalent (GED)"),
+         ppeduc = fct_relevel(ppeduc, "Never graduated high school", 
+                              "Some college, no degree", 
+                              "High school graduate", 
+                              "Associate degree", 
+                              "Bachelors degree", 
+                              "Masters degree", 
+                              "Professional or Doctorate degree")) %>% 
+  mutate(marry_age = as.numeric(Q21D_Year) - (2017 - ppage))
+
+  colnames(couples) <- gsub('hcm2017q24_', '', colnames(couples))
+  
+  gathered_couples <- 
+    couples %>% 
+    gather(`_school`:`_met_online`, 
+           key = "meeting_type", 
+           value = "value") %>% 
+    filter(value == "yes")
+  
+  # Remove in final script
+  
+  gathered_couples %>% count(meeting_type) %>% arrange(desc(n))
+
+couples_orig <- read.dta13("HCMST_ver_3.04.dta")
 
 # Create temp file to write the R data structure to
 
@@ -17,3 +43,4 @@ couples_file <- tempfile()
 # Write the R data structure to the temporary file
 
 write_rds(couples, "how_couples_meet/couples_file.rds")
+
